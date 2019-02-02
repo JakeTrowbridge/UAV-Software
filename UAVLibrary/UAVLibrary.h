af5f965a3b12a525eaffc353439b9955531265f4
JakeTrowbridge <47070726+JakeTrowbridge@users.noreply.github.com>
2019-02-02 17:52:25 +0000

    Created UAV Library header file
    This file provides definitions for the library: a listing of all variables and functions etc.

---
+++ UAVLibrary/UAVLibrary.h
@@ -0,0 +1,9 @@
+/* 
+UAVLibrary.h - custom sensor library for UoS Part II UAV project
+Created by Jake S and Jake T, February 2019
+*/
+
+#indef UAVLibrary_h
+#define UAVLibrary_h
+
+#include "Arduino.h"
