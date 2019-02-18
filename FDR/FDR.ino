68db7d78da2903e6d9a58424b7bdff3ca7dddc9b
JakeTrowbridge <47070726+JakeTrowbridge@users.noreply.github.com>
2019-02-18 00:11:33 +0000

    Added pressure altitude sensor and corrections

--- FDR/FDR.ino
+++ FDR/FDR.ino
@@ -8,10 +8,14 @@ Flight Data Recorder Code Here!
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_L3GD20_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_LSM303.h>
+#include <Adafruit_LSM303_U.h>
 
 //sensors
 Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
-Adafruit_LIS3DH accel = Adafruit_LIS3DH();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 
 /* alignmant
  * X is Forward
@@ -30,7 +34,7 @@ float allDat[15]; //format: [0Time, 1Pitch, 2Roll, 3Heading, 4PressAlt, 5GPSLong
                   // 7GPSAlt, 8GPSSpeed, 9Ch1, 10Ch2, 11Ch3, 12Ch4, 13Ch5, 14Ch6]
 
 //~~~~~~~~~~~~~~Adjustable Variables~~~~~~~~~~~~~~~~~~
-int sampleTime = 200;       // minimum time between samples in ms
+int sampleTime = 50;       // minimum time between samples in ms
 float P_CompCoeff = 0.975;  // Pitch complimentary coefficient
 float R_CompCoeff = 0.975;  // Roll complimentary coefficient
 
@@ -49,13 +53,19 @@ void setup() {
     Serial.println("Sd initialization failed!");
     while (1);
   }
+  if(!bmp.begin())
+  {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    Serial.println("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
     for (int i = 2; i < 8; i++){  // set pins D2-D7 as input pins for receiver
     pinMode(i, INPUT);
   }
   Serial.println("Initialization done.");
   gyro.enableAutoRange(true);
   accel.enableAutoRange(true);
-  }
+}
 
 //~~~~~~~~~~~~~~~~~~~~~Main Loop~~~~~~~~~~~~~~~~~~~~
 void loop(){
@@ -67,8 +77,15 @@ void loop(){
     updateSensorData();
     updateReceiverData();
     sdWrite();
-  }
-    
+
+//  Serial.print(allDat[0]); //Serial write for testing
+//  Serial.print(", ");
+//  Serial.print(allDat[1]);
+//  Serial.print(", ");
+//  Serial.print(allDat[2]);
+//  Serial.print(", ");
+//  Serial.println(allDat[4]);
+  }  
 }
 
 //~~~~~~~~~~~~~~~~~~~Functions~~~~~~~~~~~~~~~~~~~~~~
@@ -78,7 +95,7 @@ void updateSensorData(){ // get sensor data over I2C
     float AX = event.acceleration.x;
     float AY = event.acceleration.y;
     float AZ = event.acceleration.z;
-  
+    
     gyro.getEvent(&event);  // get gyro data
     float RX = event.gyro.x;
     float RY = event.gyro.y;
@@ -89,11 +106,15 @@ void updateSensorData(){ // get sensor data over I2C
     
     allDat[1] = pitch;
     allDat[2] = roll;
+
+    bmp.getEvent(&event);
+    allDat[4] = bmp.pressureToAltitude(1015, event.pressure);
 }
+
 
 float P_ComplementaryFilter(float ax,float ay,float az,float rx,float ry,float rz) {    // pitch complimentary filter
   long squaresum=(long)ay*ay+(long)az*az;
-  pitch+=((-ry/32.8f)*(dt/1000000.0f));
+  pitch+=((-rx/32.8f)*(dt/1000000.0f));
   float pitchAcc =atan(ax/sqrt(squaresum))*RAD_TO_DEG;
   pitch =P_CompCoeff*pitch + (1.0f-P_CompCoeff)*pitchAcc;
   return(pitch);
@@ -101,13 +122,13 @@ float P_ComplementaryFilter(float ax,float ay,float az,float rx,float ry,float r
 
 float R_ComplementaryFilter(float ax,float ay,float az,float rx,float ry,float rz) {    // roll complimentary filter
   long squaresum=(long)ax*ax+(long)az*az;
-  roll+=((-rx/32.8f)*(dt/1000000.0f));
+  roll+=((-ry/32.8f)*(dt/1000000.0f));
   float rollAcc =atan(ay/sqrt(squaresum))*RAD_TO_DEG;
   roll =R_CompCoeff*roll + (1.0f-R_CompCoeff)*rollAcc;
   return(roll);
 }
 
-void SDwrite(){   // Write data in allDat to SD card
+void sdWrite(){   // Write data in allDat to SD card
   dataFile = SD.open("datatest.csv", FILE_WRITE);
   if (dataFile) {
     for(int i = 0; i < 15; i++){
@@ -119,6 +140,8 @@ void SDwrite(){   // Write data in allDat to SD card
     Serial.println("Error opening file");
   }
   dataFile.close();
+}
+
     
 void updateReceiverData(){   // get pulse lengths from receiver in ms
   for (int x = 2; x < 8; x++){
