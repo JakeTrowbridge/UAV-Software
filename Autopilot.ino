257d70953a5b50bb8177d58166e01f088b849fb5
JakeTrowbridge <47070726+JakeTrowbridge@users.noreply.github.com>
2019-03-12 12:57:37 +0000

    Changed altitude PID and added Roll PID
    Not tested yet

--- Autopilot.ino
+++ Autopilot.ino
@@ -8,28 +8,41 @@ const byte MY_ADDRESS = 42;
 volatile boolean haveData = false;
 
 //~~~~~~~~~~~~~~~~~~~~Variable~~~~~~~~~~~~~~~~~~~~~
-float Elv_target = 0;
-float Elv_current = 0;
-float Elv_error = 0;
-float prevElv_error = 0;
-float Elv_integral = 0;
-float Elv_derivative = 0;
+float Alt_target = 0;
+float Alt_current = 0;
+float Alt_error = 0;
+float Alt_prevError = 0;
+float Alt_integral = 0;
+float Alt_derivative = 0;
+float Elv_pos = 0;
 
-float Elv_kp = 10;
-float Elv_ki = 0;
-float Elv_kd = 0;
+float Roll_target = 0;
+float Roll_current = 0;
+float Roll_error = 0;
+float Roll_prevError = 0;
+float Roll_integral = 0;
+float Roll_derivative = 0;
+float Ail_pos = 0;
 
 long t = 0;
 int dt = 0;
 
-float allDat[15]; //format: [0Time, 1Pitch, 2Roll, 3Heading, 4PressAlt, 5GPSLong, 6GPSLat,
-                  // 7GPSAlt, 8GPSSpeed, 9Ch1, 10Ch2, 11Ch3, 12Ch4, 13Ch5, 14Ch6]
+float allDat[9]; //format: [0Time, 1Pitch, 2Roll, 3Heading, 4PressAlt, 5AutoPilot, 6GPSLong, 7GPSLat,8GPSSpeed]
+
+//~~~~~~~~~~~~~~~~Changable Variables~~~~~~~~~~~~
+float Alt_kp = 1;
+float Alt_ki = 0;
+float Alt_kd = 0;
+
+float Roll_kp = 1;
+float Roll_ki = 0;
+float Roll_kd = 0;
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 void receiveEvent(int howMany)// called by interrupt service routine when incoming data arrives
  {
- if (howMany >= (sizeof allDat))
+ if (howMany >= (32))
    {
    I2C_readAnything (allDat); 
    haveData = true;     
@@ -37,56 +50,83 @@ void receiveEvent(int howMany)// called by interrupt service routine when incomi
  
 } // end of receiveEvent
 
-//~~~~~~~~~~~~~~~~~~~~~ServoSetup~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Servo Elv_Servo;
-Servo Alr_Servo;
+//~~~~~~~~~~~~~~~~~~~~~ServoSetup~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Servo Elv_servo;
+  Servo Ail_servo;
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~Setup~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~Setup~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void setup(){
   Wire.begin (MY_ADDRESS);
-  Serial.begin (9600);
+  Serial.begin (115200);
   Wire.onReceive (receiveEvent);
-  Elv_Servo.attach(12);
-  Alr_Servo.attach(11);
+
+  Elv_servo.attach(9);
+  Ail_servo.attach(10);
 }  // end of setup
 
 void loop(){
-//~~~~~~~~~~~~~~~~~~~~~~~~~I2C comunication~~~~~~~~~~~~~~~~~~~  
-  if (haveData)
-    {
-     for(int i=0;i<15;i++){
-      Serial.print(allDat[i]); Serial.print(", ");
-     }
-      Serial.println();
+//~~~~~~~~~~~~~~~~~~~~~~~~~I2C comunication~~~~~~~~~~~~~~~~~~  
+//  if (haveData)    {
+//     for(int i=1;i<9;i++){
+//      Serial.print(allDat[i]); Serial.print(", ");
+//     }
+//      Serial.println();
     haveData = false;
-    }  // end if haveData
-
-//~~~~~~~~~~~~~~~~~~~~~PID~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  dt = millis() - t;
-  if (dt > 50){
-    t = millis();
-    Elv_target = allDat[1];
-    
-    Elv_error = Elv_target - Elv_current;   //P
-
-    Elv_integral = Elv_integral + (Elv_error * dt);
-    if (Elv_error < 0.1){
-      Elv_integral = 0;
+//    }  // end if haveData
+//~~~~~~~~~~~~~~~~~~~~~~~~Auto Pilot~~~~~~~~~~~~~~~~~~~~~~~~~
+  if(allDat[5]==0){
+    Alt_target = 0;
+  }
+  else if(allDat[5]==1){
+    if (Alt_target == 0){
+      Alt_target = allDat[4];
     }
-    if (abs(Elv_error) > 40){
-      Elv_integral = 0;
+    dt = millis() - t;
+    if (dt > 50){
+      t = millis();
+//~~~~~~~~~~~~~~~~~~~~~~~~Altitude Stabilization~~~~~~~~~~~~~~~~      
+      Alt_current = Alt_current * 0.3 + allDat[4] * 0.7;
+      Alt_error = Alt_target - Alt_current;
+  
+      Alt_integral = Alt_integral + (Alt_error * dt);
+      if (Alt_error < 0.1){
+        Alt_integral = 0;
+      }
+      if (abs(Alt_error) > 40){
+        Alt_integral = 0;
+      }
+  
+      Alt_derivative = (Alt_error - Alt_prevError)/dt;
+      Alt_prevError = Alt_error;
+      Elv_pos =(Alt_kp * Alt_error + Alt_ki * Alt_integral + Alt_kd * Alt_derivative)/dt;
+      Serial.print(allDat[4]);
+      Serial.print(", ");
+      Serial.print(Alt_current);
+      Serial.print(", ");
+      Serial.println(Elv_pos);
+      Elv_servo.write(map(Elv_pos, -90, 90, 150, 30));
+
+ //~~~~~~~~~~~~~~~~~~~~~~~Roll Stabilization~~~~~~~~~~~~~~~~~~~~~
+      Roll_current = Roll_current * 0.3 + allDat[2] * 0.7;
+      Roll_error = Roll_target - Roll_current;
+  
+      Roll_integral = Roll_integral + (Roll_error * dt);
+      if (Roll_error < 0.1){
+        Roll_integral = 0;
+      }
+      if (abs(Roll_error) > 40){
+        Roll_integral = 0;
+      }
+      Roll_derivative = (Roll_error - Roll_prevError)/dt;
+      Roll_prevError = Roll_error;
+      Ail_pos =(Roll_kp * Roll_error + Roll_ki * Roll_integral + Roll_kd * Roll_derivative)/dt;
+      Serial.print(allDat[2]);
+      Serial.print(", ");
+      Serial.print(Roll_current);
+      Serial.print(", ");
+      Serial.println(Ail_pos);
+      Ail_servo.write(map(Ail_pos, -90, 90, 150, 30));
+      
     }
-
-    Elv_derivative = (Elv_error - prevElv_error)/dt;
-    prevElv_error = Elv_error;
-    Elv_current = Elv_current + (Elv_kp * Elv_error + Elv_ki * Elv_integral + Elv_kd * Elv_derivative)/dt;
-    Serial.print(Elv_target);
-    Serial.print(", ");
-    Serial.println(Elv_current);
-    Elv_Servo.write(map(Elv_current, -90, 90, 180, 0));
-}
-}  // end of loop
-
-
-
+  }
+} // end of loop
