a607e3b7802dbfe119aef33619bd1ef906340233
JakeTrowbridge <47070726+JakeTrowbridge@users.noreply.github.com>
2019-03-20 00:00:46 +0000

    Delete UAVLibrary.h

--- UAVLibrary/UAVLibrary.h
+++
@@ -1,9 +0,0 @@
-/* 
-UAVLibrary.h - custom sensor library for UoS Part II UAV project
-Created by Jake S and Jake T, February 2019
-*/
-
-#indef UAVLibrary_h
-#define UAVLibrary_h
-
-#include "Arduino.h"
